d51f6ace67baf606fb68fb05645a39f3af3ee787
Aaron-Muuo <aaronmuuo60@gmail.com>
2022-07-08 18:29:55 +0300

    MPesa application - LipaNaMpesaOnline

---
+++ process.php
@@ -0,0 +1,71 @@
+<?php
+
+    $phone = $_POST["phone"];
+    $amount = $_POST["amount"];
+
+    //endpoints url
+    $stk_request_url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
+    $outh_url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
+
+
+    //provided keys and codes
+    $safaricom_pass_key = ""; //get passkey from portal
+    $safaricom_bussiness_short_code = "174379";
+    $safaricom_party_b = "174379";
+    $safaricom_Consumer_Key = ""; //get consumer key from portal
+    $safaricom_Consumer_Secret = ""; //get consumer secret from portal
+
+    //combine to get an access token
+    $outh = $safaricom_Consumer_Key . ':' . $safaricom_Consumer_Secret;
+
+    //network calls using curl to get authorization
+    $curl_outh = curl_init($outh_url);
+    curl_setopt($curl_outh, CURLOPT_RETURNTRANSFER, 1);
+    $credentials = base64_encode($outh);
+    curl_setopt($curl_outh, CURLOPT_HTTPHEADER, array('Authorization: Basic ' . $credentials));
+    curl_setopt($curl_outh, CURLOPT_HEADER, false);
+    curl_setopt($curl_outh, CURLOPT_SSL_VERIFYPEER, false);
+
+    $curl_outh_response = curl_exec($curl_outh);
+    $json = json_decode($curl_outh_response, true);
+
+    //generate time
+    $time = date("YmdHis", time());
+
+    //generate password
+    $password = $safaricom_bussiness_short_code . $safaricom_pass_key . $time;
+    $transaction_password = base64_encode($password);
+    
+    //stk
+    $curl_stk = curl_init();
+
+    curl_setopt($curl_stk, CURLOPT_URL, $stk_request_url);
+    curl_setopt($curl_stk, CURLOPT_HTTPHEADER, array('Content-Type:application/json', 'Authorization:Bearer ' . $json['access_token']));
+    
+    //prepare the data to send
+    $curl_post_data = array(
+        "BusinessShortCode"=> $safaricom_bussiness_short_code,
+        "Password"=> $transaction_password,
+        "Timestamp"=> $time,
+        "TransactionType"=>"CustomerPayBillOnline",
+        "Amount"=> $amount,
+        "PartyA"=> $phone,
+        "PartyB"=> $safaricom_party_b,
+        "PhoneNumber"=> $phone,
+        "CallBackURL"=>"https://mydomain.com/path",
+        "AccountReference"=>"Ronnabble Digital LTD",
+        "TransactionDesc"=>"Test payment"
+    );
+
+    $data_string = json_encode($curl_post_data);
+    curl_setopt($curl_stk, CURLOPT_POST, 1);
+    curl_setopt($curl_stk, CURLOPT_POSTFIELDS, $data_string);
+    curl_setopt($curl_stk, CURLOPT_RETURNTRANSFER, 1);
+
+    //execute and close connection
+    $response = curl_exec($curl_stk);
+    curl_close($curl_stk);
+
+   // returrn response
+    echo $response;
+?>
